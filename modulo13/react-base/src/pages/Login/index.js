e0cc52f18d95fcc036c81d2eb1b00d25f213dca6
vitor <vitorcarneiro778@gmail.com>
2022-04-01 16:31:27 -0300

    update

--- modulo13/react-base/src/pages/Login/index.js
+++ modulo13/react-base/src/pages/Login/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Titulo, Paragrafo } from './styled';
+import * as exampleActions from '../../store/modules/example/actions';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch(exampleActions.clicaBotao());
+  }
+
   return (
     <Container>
       <Titulo>
@@ -12,7 +22,9 @@ export default function Login() {
       </Titulo>
       <Paragrafo>Id duis nostrud ullamco sint.</Paragrafo>
       <a href="none">Oieeee</a>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
